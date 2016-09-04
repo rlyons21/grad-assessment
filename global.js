f88a0fd0a7d2cc2e00d217db258092d94aa58608
Rachel Lyons <rlyons21@hotmail.com>
2016-09-04 10:24:12 -0500

    changes number of likes when click like/unlike

--- global.js
+++ global.js
@@ -1,12 +1,21 @@
 window.addEventListener("load", function(){
 	var like_btn = document.getElementsByClassName("like");
+	var like_count = document.getElementsByClassName("likeCount");
+	debugger;
 
 	for(i=0; i < like_btn.length; i++){
-		like_btn[i].addEventListener("click", function(){
+		like_btn[i].addEventListener("click", function(e){
+			debugger;
+			var index = parseInt(this.getAttribute("id"));
+			var count = parseInt(like_count[index].innerHTML);
+			debugger;
 			if(this.innerHTML == "Like"){
 				this.innerHTML = "Unlike";
+				like_count[index].innerHTML = count + 1;
+				
 			} else{
 				this.innerHTML = "Like";
+				like_count[index].innerHTML = count - 1;
 			}
 		});
 	};
